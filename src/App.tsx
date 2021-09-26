c7dc4bd364be4615b8aa96c82b836509cd01b4a9
jeronimoek <jeronimoek@gmail.com>
2021-09-26 01:12:08 -0300

    Added scss and fixed warnings.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import './App.css';
+import './App.scss';
 import SearchBar from './app/components/SearchBar'
 import JobListing from './app/components/JobListing'
 import ApolloClient, { gql, OperationVariables }from 'apollo-boost';
--- src/app/components/SourceCode.tsx
+++ src/app/components/SourceCode.tsx
@@ -3,7 +3,7 @@ import { GithubOutlined } from "@ant-design/icons"
 const SourceCode = (): JSX.Element => {
   return(
     <div className="SourceCode">
-      <a target="_blank" href="https://github.com/jeronimoek/search-jobs-react-app">
+      <a target="_blank" rel="noreferrer" href="https://github.com/jeronimoek/search-jobs-react-app">
         <GithubOutlined style={{fontSize:70}}/>
       </a>
     </div>
